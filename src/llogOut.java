e6a05059f70186fd49e50d35765e465d415314d8
usnabeel1 <73013767+usnabeel1@users.noreply.github.com>
2020-10-17 13:02:21 +0300

    Update llogOut.java

--- src/llogOut.java
+++ src/llogOut.java
@@ -1,6 +1,6 @@
 public class llogOut extends Homepage {
     public boolean logOut(String Username){
-        System.out.println(this.Username + "are you sure you want to leave us");
+        System.out.println(this.Username + "are you sure you want to leave us?"); //edited by usnabeel1; added a question mark to "are you sure you want to leave us
         return true;
     }
 }
